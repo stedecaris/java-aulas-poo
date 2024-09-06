fa49e2dd8aa75661d66de1e5150c620391ca9c0a
stephany <stephanydecaris@gmail.com>
2024-09-06 09:58:59 -0300

    primeiro commit

---
+++ src/main/java/br/sesi/sdf/java_git_poo/JavaGitPooApplication.java
@@ -0,0 +1,13 @@
+package br.sesi.sdf.java_git_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGitPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGitPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/sesi/sdf/java_git_poo/JavaGitPooApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sesi.sdf.java_git_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGitPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
